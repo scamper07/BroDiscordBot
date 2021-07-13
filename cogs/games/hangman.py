ba418d80101201d576049230c2f0fc2a5e6fe11a
pavan <pavanvenkatesh94@gmail.com>
2021-07-13 12:22:42 -0700

    - hangman now supports  running multiple game instances in different servers at the same time - added constants file

--- cogs/games/hangman.py
+++ cogs/games/hangman.py
@@ -1,25 +1,24 @@
-import aiohttp
 from discord.ext import commands
-from discord_slash import cog_ext
-from discord_slash.utils import manage_commands
-from discord_slash.utils.manage_commands import create_choice
 
-from base_logger import logger
+import constants
 
 
 class HM:
     # Manages one instance of Hangman game at a given time
-    def __init__(self):
+    def __init__(self, server_id, channel_id, challenger):
+        self.server_id = server_id
+        self.channel_id = channel_id
+        self.challenger = challenger
+        self.accepting_letters = False
         self.truth = None
         self.guessed_letters = []
         self.wrong_answers = 0
         self.blanks = []
         self.hangman_status = ""
-        self.game_in_progress = False
         self.is_game_over = False
         self.has_player_won = False
-        self.print_list = [".       |", ".     :dizzy_face:", ".:point_up:", ":coat:",":point_up:",
-                           " .     :jeans:", ]
+        self.print_list = [constants.EMOJI_HANGMAN_ROPE, ".     " + constants.EMOJI_DIZZY_FACE, "." + constants.EMOJI_POINT_FINGER_UP, constants.EMOJI_COAT, constants.EMOJI_POINT_FINGER_UP,
+                           " .     " + constants.EMOJI_JEANS ]
 
     def process_input(self, letter):
         if self.is_game_over:
@@ -35,7 +34,7 @@ class HM:
     def update_blanks(self, letter):
         if letter == "":
             for _ in self.truth:
-                self.blanks.append(":question:")
+                self.blanks.append(constants.EMOJI_QUESTION_MARK)
         else:
             for i, l in enumerate(self.truth):
                 if l.upper() == letter.upper():
@@ -56,13 +55,13 @@ class HM:
     def get_game_status(self):
         result_string = ""
         if self.is_game_over:
-            result_string += "\n :regional_indicator_g: :regional_indicator_a: :regional_indicator_m: :regional_indicator_e:      :regional_indicator_o: :regional_indicator_v: :regional_indicator_e: :regional_indicator_r: \n "
+            result_string += constants.STR_GAME_OVER
             if self.has_player_won:
-                result_string += ":regional_indicator_y: :regional_indicator_o: :regional_indicator_u:     :regional_indicator_w: :regional_indicator_o: :regional_indicator_n:"
+                result_string += constants.STR_YOU_WON
             else:
-                result_string += ":regional_indicator_y: :regional_indicator_o: :regional_indicator_u:     :regional_indicator_l: :regional_indicator_o: :regional_indicator_s: :regional_indicator_t:" + "\n" + "The word was " + self.truth
+                result_string += constants.STR_YOU_LOST + "\n" + "The word was " + self.truth
         else:
-            result_string = "Guessed so far : "
+            result_string = constants.STR_GUESSED_SO_FAR
             guesses = self.guessed_letters
             for i in range(len(guesses)):
                 result_string += guesses[i].upper() + " , "
@@ -70,59 +69,71 @@ class HM:
         blanks = ""
         for b in self.blanks:
             blanks += b + "\t"
-        return "\n" + blanks + " \n " + self.hangman_status + "\n" + result_string
+        return "\n" + blanks + " \n" + self.hangman_status + "\n" + result_string
 
 
 class Hangman(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.channel_id = None
-        self.accepting_letter = False
-        self.hangman = HM()
+        self.game_instances = {}
+        self.challenger_to_server_dict = {}
 
     @commands.command(brief="Hangman game")
     async def hangman(self, ctx):
-        # dm player1
+        # dm challenger
         # Create a hangman instance
         # print on channel
-        if self.hangman.game_in_progress:
-            await ctx.channel.send("I am already playing a game")
+        server_id = ctx.guild.id
+        channel_id = ctx.channel
+        challenger = ctx.author.id
+        if server_id in self.game_instances:
+            await ctx.channel.send(constants.STR_ALREADY_PLAYING)
             return
-        self.hangman.game_in_progress = True
-        self.channel_id = ctx.channel
-        player_one = ctx.author.id
-        user = self.bot.get_user(player_one)
-        await user.send("Tell me the secret word, type \".word [your word]\" ")
+        else:
+            self.game_instances[server_id] = HM(server_id, channel_id, challenger)
+        user = self.bot.get_user(challenger)
+        self.challenger_to_server_dict[challenger] = server_id
+        await user.send(constants.STR_GET_WORD)
 
     @commands.command()
     async def word(self, ctx, word=""):
-        if not self.hangman.game_in_progress:
-            ctx.channel.send("Please use .hangman before using the word command")
+        challenger = ctx.author.id
+        if challenger not in self.challenger_to_server_dict:
+            await ctx.channel.send(constants.STR_TRY_HANGMAN)
             return
+        server_id = self.challenger_to_server_dict[challenger]
+        hm_instance = self.game_instances[server_id]
         if word == "":
-            ctx.channel.send("Please type your word with the command Eg: .word [your word]")
-        self.accepting_letter = True
-        self.hangman.truth = word.upper()
-        self.hangman.process_input("")
-        await self.channel_id.send(self.hangman.get_game_status())
+            await ctx.channel.send(constants.STR_WORD_EMPTY)
+            return
+        hm_instance.accepting_letters = True
+        hm_instance.truth = word.upper()
+        hm_instance.process_input("")
+        await hm_instance.channel_id.send(hm_instance.get_game_status())
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if not message.guild:
+            return
+        channel_id = message.channel
+        server_id = message.guild.id
         if message.author == self.bot.user:
             return
-        if not self.hangman.game_in_progress:
+        if server_id not in self.game_instances:
             return
-        if not self.accepting_letter:
+        hm_instance = self.game_instances[server_id]
+        if channel_id != hm_instance.channel_id:
+            return
+        if not hm_instance.accepting_letters:
             return
         if len(message.content) != 1:
             return
-        self.accepting_letter = False
-        self.hangman.process_input(message.content)
-        await message.channel.send(self.hangman.get_game_status())
-        self.accepting_letter = True
-        if self.hangman.is_game_over:
-            self.hangman.game_in_progress = False
-            self.hangman = HM()
+        hm_instance.accepting_letters = False
+        hm_instance.process_input(message.content)
+        await hm_instance.channel_id.send(hm_instance.get_game_status())
+        hm_instance.accepting_letters = True
+        if hm_instance.is_game_over:
+            del self.game_instances[server_id]
 
 
 def setup(bot):
