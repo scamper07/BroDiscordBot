fe2d3ffcf001a1f14098bceb2dd25008bd5381c8
pavan <pavanvenkatesh94@gmail.com>
2021-09-30 00:59:23 -0700

    Added round robin round

--- cogs/games/tlto.py
+++ cogs/games/tlto.py
@@ -17,6 +17,9 @@ class Tlto(commands.Cog):
         self.organizer_dm = ""
         self.pounce_answers_counter = 0
         self.current_scores = collections.defaultdict(int)
+        # TODO : make this dynamic. Naanu's Sai kumar helps ? or player can use a command .signup
+        self.players = {"Suhan", "Kunal", "Sai", "Vatican Cameos", "scamper"}
+        self.who_pounced = set()
 
     @commands.command(brief="Start quiz")
     @commands.has_role("Organizer")
@@ -37,10 +40,12 @@ class Tlto(commands.Cog):
                     scores = self.score_round(messages)
                     # Normal round scoring ? wait ?
                     res = self.score(scores)
+                    await self.round_robin()
                     await ctx.send(res)
                 else:
                     self.is_pounce_enabled = True
                     print("Pounce enabled")
+                    self.who_pounced = set()
                     self.pounce_answers_counter = 0
 
     @commands.Cog.listener()
@@ -59,10 +64,29 @@ class Tlto(commands.Cog):
                 self.pounce_answers_counter += 1
                 u_organizer = self.bot.get_user(self.organizer)
                 user_m = await u_organizer.send(message.author.name + " : " + message.content)
+                self.who_pounced.add(message.author.name)
                 await asyncio.sleep(.75)
                 for emoji in self.emojis:
                     await user_m.add_reaction(emoji)
+                    # TODO :: check if we can get hold of click events on emoji to notify if the sender of this
+                    #  message was right or wrong
                     await asyncio.sleep(.75)
+
+    async def round_robin(self):
+        # List all the players who did not pounce
+        players = self.players - self.who_pounced
+        u_organizer = self.bot.get_user(self.organizer)
+        for player in players:
+            user_m = await u_organizer.send(player)
+            # React with just a tick
+            await user_m.add_reaction(self.emojis[0])
+
+        # On click of a tick score for that particular person
+        # (Optional)
+        # If the QM made a mistake checking the wrong person, he is allowed to tick another person
+        # The latest tick before the next pounce will be considered for the score.
+        # (/ Optional)
+        # Update and print scores
 
     def score(self, scores):
         # TODO :: add to final score
