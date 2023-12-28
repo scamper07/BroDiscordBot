245d9fd9e0b539b23b53b8c65c114ab24678585e
scamper07 <preetham.kulai21@gmail.com>
2023-12-29 00:46:18 +0530

    minor refactor

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,8 +1,8 @@
 import pytest
-from src.cogs.misc.general import General
+from src.cogs.misc.general import UtilGeneral
 
 
 @pytest.mark.asyncio
 async def test_get_xkcd_comic():
-    json_response = await General.get_xkcd_comic()
+    json_response = await UtilGeneral.get_xkcd_comic()
     assert type(json_response["title"]) is str and type(json_response["img"]) is str
